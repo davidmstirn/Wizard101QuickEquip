5cd930f2d5bac02be8061efe7ec1f5d439b380af
David Stirn <davidmstirn@gmail.com>
2022-05-23 19:38:09 -0400

    Fixed import and added more thorough testing

--- client/src/tests/WizardSelect.spec.js
+++ client/src/tests/WizardSelect.spec.js
@@ -1,21 +1,29 @@
 import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import WizardSelect from '../components/WizardSelect.js';
+import { BrowserRouter } from 'react-router-dom';
+import WizardSelect from '../routes/WizardSelect.js';
 
 describe('<WizardSelect />', () => {
   describe('clicking the first wizard', () => {
     beforeEach(async () => {
       let wizards = [
-        { name: "Matthew Firebringer", school: "Fire", level: 150 }
+        { id: 0, name: "Matthew Firebringer", school: "Fire", level: 150 },
+        { id: 1, name: "Artur Lifehaven", school: "Life", level: 150 }
       ]
 
-      render(<WizardSelect wizards={wizards} />);
-
-      userEvent.click(screen.getByTestId('wizardOption0'));
+      render(<BrowserRouter><WizardSelect wizards={wizards} /></BrowserRouter>);
     });
 
-    it('selects the wizard', () => {
+    it('selects the second wizard', () => {
+      userEvent.click(screen.getByTestId('wizardOption1'));
+      expect(screen.getByTestId('wizardOption1').checked).toEqual(true);
+      expect(screen.getByTestId('wizardOption0').checked).toEqual(false);
+    });
+
+    it('selects the first wizard', () => {
+      userEvent.click(screen.getByTestId('wizardOption0'));
       expect(screen.getByTestId('wizardOption0').checked).toEqual(true);
+      expect(screen.getByTestId('wizardOption1').checked).toEqual(false);
     });
   });
 });
