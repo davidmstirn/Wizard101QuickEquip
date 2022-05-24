1e02d8402e562f9bf8f5a6c519ae52bb18635a0e
David Stirn <davidmstirn@gmail.com>
2022-05-24 15:57:04 -0400

    Added convenience script for development

---
+++ open-workspace.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+start cmd /K "cd /d D:\Dev\Node\Wizard101QuickEquip\client && npm run start"
+start cmd /K "cd /d D:\Dev\Node\Wizard101QuickEquip\client && npm run test"
+start cmd /K "cd /d D:\Dev\Node\Wizard101QuickEquip\client && npm run cypress"
